16c56aa2503fbf3e6b8123c023ffff5099200526
Servando Miguel <servandomiguel@gmail.com>
2018-12-04 22:40:49 -0600

    Json!

--- twitter_api.py
+++ twitter_api.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import tweepy
 import auth_credentials as cred
+import json
 
 def authenticate():
     auth = tweepy.OAuthHandler(cred.consumer_key, cred.consumer_secret)
@@ -9,11 +13,14 @@ def authenticate():
 def getAllTweets(account_name):
     return [tweet for tweet in tweepy.Cursor(api.user_timeline,id=account_name).items()]
 
+def dumpTweetToJson(tweet):
+    return json.dumps(tweet._json)
 if __name__ == '__main__':
     api = authenticate()
     # Prueba de autenticación con tweepy
     user = api.me()
     print(user.name)
     print('@' + api.me().screen_name)
-
-    print 'hola'
+    l = getAllTweets('espnmx')
+    print dumpTweetToJson(l[0])
+    print 
